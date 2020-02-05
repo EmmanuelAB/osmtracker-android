afb1f204f16122d311285395191132499e2496cc
Emmanuel Alfaro Brenes <ealfarobrenes@hotmail.com>
2020-02-05 10:48:10 -0600

    seconds

--- app/src/androidTest/java/net/osmtracker/activity/TrackManagerTest.java
+++ app/src/androidTest/java/net/osmtracker/activity/TrackManagerTest.java
@@ -1,17 +1,23 @@
 package net.osmtracker.activity;
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.matcher.PreferenceMatchers;
 import android.support.test.rule.ActivityTestRule;
+import android.util.Log;
 
 import net.osmtracker.R;
 
 import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.*;
 import static android.support.test.espresso.action.ViewActions.*;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.PreferenceMatchers.withSummary;
+import static android.support.test.espresso.matcher.PreferenceMatchers.withSummaryText;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
 
 
@@ -19,20 +25,32 @@ import static org.junit.Assert.*;
 
 public class TrackManagerTest {
 
+    private void log(String s){Log.e("#",">>> "+s);}
+
     @Rule
     public ActivityTestRule<TrackManager> mRule = new ActivityTestRule<>(TrackManager.class);
 
     @Test
     public void test(){
-        onView(withText("footrack")).perform(click());
 
-        onView(withId(R.id.trackdetail_item_description)).perform(typeText("foo bar text"));
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
 
-        onView(withText("SAVE")).perform(click());
+        onView(withText("Settings")).perform(click());
 
-        onView(withText("footrack")).perform(click());
+        onView((withText("Voice record duration"))).perform(click());
 
-        onView(withId(R.id.trackdetail_item_description)).check(matches(withText("foo bar text")));
+        onView((withText("10"))).perform(click());
+
+        Espresso.pressBack();
+
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        onView(withText("Settings")).perform(click());
+
+        PreferenceMatchers.withTitleText("Voice record duration").matches(withSummaryText("10 seconds"));
+
+
+
 
 
 
